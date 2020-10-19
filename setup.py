627174c172fd21c3c341d0dd3eae81313a3cf9a2
David Cabrero <david.cabrero@madsgroup.org>
2020-10-19 12:47:41 +0200

    Añadido el script ejecutable como tal

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name= 'p1 libs',
     version= '0.1.0',
+    scripts= ['atspi_dump.py'],
     packages= find_packages(include=['p1', 'p1.*'])
 )
