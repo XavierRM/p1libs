d48c44d758ee9f9aa57ccb854cd149ecb66f355d
David Cabrero <david.cabrero@madsgroup.org>
2020-10-19 12:56:38 +0200

    Actualizada version number en setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name= 'p1 libs',
-    version= '0.1.0',
+    version= '0.1.1',
     scripts= ['atspi_dump.py'],
     packages= find_packages(include=['p1', 'p1.*'])
 )
